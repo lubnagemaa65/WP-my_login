b4b65aad68e7be11cb4c61f3e34e156cbec0ce1c
Lubna Gemaa <71165802+lubnagemaa65@users.noreply.github.com>
2023-04-26 11:58:53 +0300

    Add files via upload

---
+++ theme-my-login-master/theme-my-login-master/src/admin/assets/scripts/metaboxes.js
@@ -0,0 +1,27 @@
+( function( $ ) {
+
+	$( initMetaBoxes );
+
+	function initMetaBoxes() {
+		var metaboxes = $( '#tml-settings .postbox' );
+
+		if ( metaboxes.length ) {
+			// Make metaboxes toggleable
+			postboxes.add_postbox_toggles( pagenow );
+
+			// Close all metaboxes by default
+			$( '.postbox' ).addClass( 'closed' );
+
+			// Find each metabox holder
+			$( '.metabox-holder' ).each( function() {
+				var holder = $( this );
+
+				// Maybe disable sorting
+				if ( holder.data( 'sortable' ) == 'off' ) {
+					holder.find( '.meta-box-sortables' ).sortable( 'destroy' );
+					holder.find( '.postbox .hndle' ).css( 'cursor', 'default' );
+				}
+			} );
+		}
+	}
+} )( jQuery );
---
+++ theme-my-login-master/theme-my-login-master/src/assets/scripts/ajax.js
@@ -0,0 +1,45 @@
+( function ( $ ) {
+
+	$( '.tml' ).on( 'submit', 'form[data-ajax="1"]', function( e ) {
+		var form = $( this ),
+			input = form.find( ':input' ),
+			submit = form.find( ':submit' ),
+			container = $( e.delegateTarget ),
+			notices = container.find( '.tml-alerts' );
+
+		e.preventDefault();
+
+		notices.empty();
+
+		input.prop( 'readonly', true );
+		submit.prop( 'disabled', true );
+
+		$.ajax( {
+			data: form.serialize() + '&ajax=1',
+			method: form.attr( 'method' ) || 'get',
+			url: form.attr( 'action' )
+		} )
+		.always( function() {
+			input.prop( 'readonly', false );
+			submit.prop( 'disabled', false );
+		} )
+		.done( function( response ) {
+			if ( response.success ) {
+				if ( response.data.refresh ) {
+					location.reload( true );
+				} else if ( response.data.redirect ) {
+					location.href = response.data.redirect;
+				} else if ( response.data.notice ) {
+					notices.hide().html( response.data.notice ).fadeIn();
+				}
+			} else {
+				notices.hide().html( response.data.errors ).fadeIn();
+			}
+		} )
+		.fail( function( jqXHR, textStatus, errorThrown ) {
+			if ( jqXHR.responseJSON.data.errors ) {
+				notices.hide().html( jqXHR.responseJSON.data.errors ).fadeIn();
+			}
+		} );
+	} );
+} )( jQuery );
